6c3906f152ea28fc8b324fdda723fb84d3f31b23
unknown <sambaalfarabi@yahoo.com>
2026-05-04 13:10:27 +0700

    refactor: change ssid params in app.cpp to lowercase

--- firmware/head/app.cpp
+++ firmware/head/app.cpp
@@ -1,20 +1,20 @@
 #include "app.h"
 #include <WiFi.h>
 
-void App::wifi_setup(const char* SSID, const char* password) {
+void App::wifi_setup(const char* ssid, const char* password) {
     Serial.begin(115200);
 
     camera.init();
 
-    WiFi.begin(SSID, password);
+    WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
     }
 
 }
 
-void App::ros_setup(const char* SSID, const char* password, const char* serverIP){
-    ros.init(SSID,  password, serverIP);
+void App::ros_setup(const char* ssid, const char* password, const char* serverIP){
+    ros.init(ssid,  password, serverIP);
 }
 
 void App::loop() {
